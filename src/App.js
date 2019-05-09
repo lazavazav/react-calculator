fbe26ed24df700629c0708467bd1de079374e8d9
lazavazav <lazav2021@gmail.com>
2019-05-09 18:57:54 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './App.css';
+import Keypad from './components/Keypad';
+import Result from './components/Result';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+    onClick = button => {
+
+      if(button === "="){
+        this.calculate()
+      }
+      else if(button === "C"){
+        this.reset()
+      }
+      else{
+        this.setState({
+          result: this.state.result + button
+        })
+      }
+    };
+
+    calculate = () => {
+      let checkResult = ''
+      if(this.state.result.includes('--')){
+        checkResult = this.state.result.replace('--','+') }
+        else {
+          checkResult = this.state.result
+        }
+        try {
+          this.setState({
+            // eslint-disable-next-line
+            result: (eval(checkResult))
+          })
+        } catch (e) {
+          this.setState({
+            result: 'error'
+          })
+        }
+      };
+    
+       reset = () => {
+         this.setState({
+           result: ""
+         })
+       };
+   
+  
+
+  render() {
+    return (
+      <div>
+        <div className="calculator">
+        <Result result={this.state.result}/>
+        <Keypad onClick={this.onClick}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
